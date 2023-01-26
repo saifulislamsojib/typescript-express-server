40a575b0ab7ca9ebe5355eacb489e01f30cba942
saifulislamsojib <saifulislam.sojib56@gmail.com>
2023-01-26 14:30:31 +0600

    service fixed

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,15 +1,15 @@
 import User from "../models/auth.model";
 import type { Auth } from "../types/auth";
 
-export const createUser = async (user: Omit<Auth, "_id">) => {
+export const createUser = (user: Omit<Auth, "_id">) => {
   const newUser = new User(user);
-  return await newUser.save();
+  return newUser.save();
 };
 
-export const getUserByEmail = async (email: string) => {
-  return await User.findOne({ email });
+export const getUserByEmail = (email: string) => {
+  return User.findOne({ email });
 };
 
-export const getUserById = async (id: string) => {
-  return await User.findOne({ id });
+export const getUserById = (id: string) => {
+  return User.findOne({ id });
 };
